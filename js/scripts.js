8123c3a20a72a9f15e9a3025bffb90b4b292dc47
Joel Delight and Maldon Meehan <joel@>
2016-05-31 11:26:51 -0700

    add js styling to fade in and out the images of the animals.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#cat").click(function() {
+    $("#fatty-katty").slideToggle();
+  });
+  $("#goat").click(function() {
+    $("#billy-goat").fadeToggle();
+  });
+  $("#dog").click(function() {
+    $("#skinny-the-dog").slideToggle();
+  });
+  $("#pig").click(function() {
+    $("#porky-the-pig").fadeToggle();
+  });
+});
