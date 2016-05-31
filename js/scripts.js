e47f808cd0305b062d9b0c1dcd88475929af2a41
Joel Delight and Maldon Meehan <maldon@>
2016-05-31 12:09:06 -0700

    add a function to toggle the display colors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
+
+  $("h3.toggle-display").click(function() {
+    $("body").removeClass();
+    $("body").addClass("reverse-color");
+    $("h3").toggle();
+  });
+
+  $("h3.toggle-display-off").click(function() {
+    $("body").removeClass();
+    $("body").addClass("normal-color");
+    $("h3").toggle();
+  });
+
+
   $("#cat").click(function() {
     $("#fatty-katty").slideToggle();
   });
+
   $("#goat").click(function() {
     $("#billy-goat").fadeToggle();
   });
+
   $("#dog").click(function() {
     $("#skinny-the-dog").slideToggle();
   });
+
   $("#pig").click(function() {
     $("#porky-the-pig").fadeToggle();
   });
